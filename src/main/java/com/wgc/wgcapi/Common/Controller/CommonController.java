5742a415d765135499bf9b5571789c197147b066
Wan Gi Jeon <dhksrl997@gmail.com>
2023-03-07 23:47:00 +0900

    feat: index,healthcheck용 Controller

--- src/main/java/com/wgc/wgcapi/Common/Controller/CommonController.java
+++ src/main/java/com/wgc/wgcapi/Common/Controller/CommonController.java
@@ -4,10 +4,17 @@ Created on 2023/02/21 11:23 PM In Intelli J IDEA
 by jeon-wangi
 */
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/common")
+@RequestMapping("/")
 public class CommonController {
+
+    @GetMapping
+    public int index() {
+        return HttpStatus.OK.value();
+    }
 }

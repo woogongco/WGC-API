4ef014d06be4992f55682b7c364fbecee006471c
seoyoon <113658373+seoyoon047@users.noreply.github.com>
2023-12-06 23:18:31 +0900

    feat: 방명록 기능 구현 (#14)

---
+++ src/main/java/com/wgc/wgcapi/Homepage/Repository/GuestBookRepositoryImpl.java
@@ -0,0 +1,33 @@
+package com.wgc.wgcapi.Homepage.Repository;
+
+import com.querydsl.core.types.Projections;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import com.wgc.wgcapi.Homepage.Entity.GuestBook;
+import com.wgc.wgcapi.Homepage.Entity.QGuestBook;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Repository
+@Transactional
+@RequiredArgsConstructor
+public class GuestBookRepositoryImpl implements CustomGuestBookRepository {
+
+    private final JPAQueryFactory query;
+    private final QGuestBook guestBook = QGuestBook.guestBook;
+
+    @Override
+    public List<GuestBook> findNonDeletedByWriterMemberId(Long writerMemberId, Long limit) {
+        return query
+                .selectFrom(guestBook)
+                .where(
+                        guestBook.writerMember.id.eq(writerMemberId),
+                        guestBook.isDelete.eq('N')
+                )
+                .orderBy(guestBook.id.desc())
+                .limit(limit)
+                .fetch();
+    }
+}

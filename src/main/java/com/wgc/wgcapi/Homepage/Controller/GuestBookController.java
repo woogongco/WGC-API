9322dd479ce97dc86b18b67e2efa9b80d7de59be
Wan Gi Jeon <dhksrl997@gmail.com>
2023-12-11 23:04:01 +0900

    guestbook -> homepage 요청path명 변경

--- src/main/java/com/wgc/wgcapi/Homepage/Controller/GuestBookController.java
+++ src/main/java/com/wgc/wgcapi/Homepage/Controller/GuestBookController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.servlet.http.HttpServletRequest;
 
 @RestController
-@RequestMapping("/guestbook")
+@RequestMapping("/homepage")
 @RequiredArgsConstructor
 public class GuestBookController {
 
